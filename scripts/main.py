f4b6705128889437d57211316a5b04ebfa94fd41
Mairon <mariusz.rogawski@gmail.com>
2021-07-09 19:03:51 +0200

    find_by_month method is done

--- scripts/main.py
+++ scripts/main.py
@@ -24,8 +24,7 @@ log_reader_json = ProfilLoggerReader(handler=json_handler)
 # print(log_reader_sqlite.load_data)
 # log_reader_txt = ProfilLoggerReader(handler=file_handler)
 # print(log_reader_txt.load_data)
-print(log_reader_json.find_by_text("info",'07-07-2021 16:00:00','07-08-2021 22:20:00')) # returns list of LogEntry that contains message: "Some info message"
-print(log_reader_json.find_by_regex(r"\w\w\w\w \w\w\w\w ", '07-07-2021 16:00:00','07-08-2021 22:20:00')) # returns list of logEntry that contains: "Some info message", "Some warning message"
-
-
-
+# print(log_reader_json.find_by_text("info",'07-07-2021 16:00:00','07-08-2021 22:20:00')) # returns list of LogEntry that contains message: "Some info message"
+# print(log_reader_json.find_by_regex(r"\w\w\w\w \w\w\w\w ", '07-07-2021 16:00:00','07-08-2021 22:20:00')) # returns list of logEntry that contains: "Some info message", "Some warning message"
+print(log_reader_json.groupby_level('07-01-2021 16:00:00','07-12-2021 22:20:00'))
+# print(log_reader_json.groupby_month('07-01-2021 16:00:00','07-12-2021 22:20:00'))
--- scripts/profil_logger_reader.py
+++ scripts/profil_logger_reader.py
@@ -1,6 +1,7 @@
 import datetime
 import sys
 import re
+import calendar
 
 
 class ProfilLoggerReader:
@@ -11,57 +12,117 @@ class ProfilLoggerReader:
         self.load_data = self.handler.read()
 
     def find_by_text(self, text, start_date=None, end_date=None):
+
         logs = []
 
-        if start_date and end_date:
-            ProfilLoggerReader.dates_validation(start_date, end_date)
-            start_date = datetime.datetime.strptime(start_date, "%d-%m-%Y %H:%M:%S")
-            end_date = datetime.datetime.strptime(end_date, "%d-%m-%Y %H:%M:%S")
-            logs = [log for log in self.load_data if text in log[2] and start_date <=
-                    datetime.datetime.strptime(log[0], "%d-%m-%Y %H:%M:%S") <= end_date]
-        else:
-            logs = [log for log in self.load_data if text in log[2]]
+        if self.load_data:
+            if start_date and end_date:
+                ProfilLoggerReader.dates_validation(start_date, end_date)
 
-        return logs
+                start_date = datetime.datetime.strptime(start_date, "%d-%m-%Y %H:%M:%S")
+                end_date = datetime.datetime.strptime(end_date, "%d-%m-%Y %H:%M:%S")
+                logs = [log for log in self.load_data if text in log[2] and start_date <=
+                        datetime.datetime.strptime(log[0], "%d-%m-%Y %H:%M:%S") <= end_date]
+            else:
+                logs = [log for log in self.load_data if text in log[2]]
+
+            return logs
+
+        else:
+            return "BRAK DANYCH DO PRACY"
 
 
     def find_by_regex(self, regex, start_date=None, end_date=None):
 
         logs = []
-        log_regex = re.compile(regex)
 
-        if start_date and end_date:
-            ProfilLoggerReader.dates_validation(start_date, end_date)
+        if self.load_data:
+            log_regex = re.compile(regex)
 
-            start_date = datetime.datetime.strptime(start_date, "%d-%m-%Y %H:%M:%S")
-            end_date = datetime.datetime.strptime(end_date, "%d-%m-%Y %H:%M:%S")
+            if start_date and end_date:
+                ProfilLoggerReader.dates_validation(start_date, end_date)
 
-            for log in self.load_data:
-                if log_regex.findall(log[2]):
-                    found_log = log_regex.findall(log[2])[0]
-                    if found_log in log[2] and \
-                            start_date <= datetime.datetime.strptime(log[0], "%d-%m-%Y %H:%M:%S") <= end_date:
-                        logs.append(log)
+                start_date = datetime.datetime.strptime(start_date, "%d-%m-%Y %H:%M:%S")
+                end_date = datetime.datetime.strptime(end_date, "%d-%m-%Y %H:%M:%S")
+
+                for log in self.load_data:
+                    if log_regex.findall(log[2]):
+                        found_log = log_regex.findall(log[2])[0]
+                        if found_log in log[2] and \
+                                start_date <= datetime.datetime.strptime(log[0], "%d-%m-%Y %H:%M:%S") <= end_date:
+                            logs.append(log)
+
+            else:
+
+                for log in self.load_data:
+                    if log_regex.findall(log[2]):
+                        found_log = log_regex.findall(log[2])[0]
+                        if found_log in log[2]:
+                            logs.append(log)
+
+            return logs
 
         else:
-
-            for log in self.load_data:
-                if log_regex.findall(log[2]):
-                    found_log = log_regex.findall(log[2])[0]
-                    if found_log in log[2]:
-                        logs.append(log)
-
-        return logs
-
+            return "BRAK DANYCH DO PRACY"
 
 
     def groupby_level(self, start_date=None, end_date=None):
-        pass
-        # return dict
+
+        if self.load_data:
+            #Tworzenie kluczy z wartosciami w formie pustych list
+            groupby_level_dict = {log[1]:[] for log in self.load_data}
+
+            #Uzupełniani wartosci listami z logami
+            if start_date and end_date:
+                ProfilLoggerReader.dates_validation(start_date, end_date)
+                start_date = datetime.datetime.strptime(start_date, "%d-%m-%Y %H:%M:%S")
+                end_date = datetime.datetime.strptime(end_date, "%d-%m-%Y %H:%M:%S")
+                groupby_level_dict = {log[1]:[] for log in self.load_data
+                                      if start_date <= datetime.datetime.strptime(log[0], "%d-%m-%Y %H:%M:%S")
+                                      <= end_date}
+
+                for log in self.load_data:
+                    if start_date <= datetime.datetime.strptime(log[0], "%d-%m-%Y %H:%M:%S") <= end_date:
+                        groupby_level_dict[log[1]].append(log)
+
+            else:
+                for log in self.load_data:
+                    groupby_level_dict[log[1]].append(log)
+
+            return groupby_level_dict
+
+        else:
+            return "BRAK DANYCH DO PRACY"
 
     def groupby_month(self, start_date=None, end_date=None):
-        pass
-        # return dict
+
+        if self.load_data:
+            groupby_month_dict = {calendar.month_name[int(log[0][3:5])]: [] for log in self.load_data}
+
+            #Uzupełniani wartosci listami z logami
+            if start_date and end_date:
+                # Tworzenie kluczy nazwami miesięcy i z wartosciami w formie pustych list
+
+                ProfilLoggerReader.dates_validation(start_date, end_date)
+                start_date = datetime.datetime.strptime(start_date, "%d-%m-%Y %H:%M:%S")
+                end_date = datetime.datetime.strptime(end_date, "%d-%m-%Y %H:%M:%S")
+                groupby_month_dict = {calendar.month_name[int(log[0][3:5])]: [] for log in self.load_data
+                                      if start_date <= datetime.datetime.strptime(log[0], "%d-%m-%Y %H:%M:%S")
+                                      <= end_date}
+
+                for log in self.load_data:
+                    if start_date <= datetime.datetime.strptime(log[0], "%d-%m-%Y %H:%M:%S") <= end_date:
+                        groupby_month_dict[calendar.month_name[int(log[0][3:5])]].append(log)
+
+            else:
+                for log in self.load_data:
+                    groupby_month_dict[calendar.month_name[int(log[0][3:5])]].append(log)
+
+            return groupby_month_dict
+
+        else:
+            return "BRAK DANYCH DO PRACY"
+
 
     @staticmethod
     def dates_validation(start_date, end_date):
